7b93727ec3699c99638e7e57aa41d7689a4214f9
Akihito Kirisaki <kirisaki@klara.works>
2024-02-16 20:28:47 +0900

    feat: indicate progress

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ fn main() {
 
     for y in 0..height {
         for x in 0..width {
+            eprint!("\rx:{:5}, y:{:5}", x, y);
+
             let r = y as f64 / ((width - 1) as f64) * 255.99;
             let g = x as f64 / ((height - 1) as f64) * 255.99;
             let b = 0.25 * 255.99;
@@ -16,4 +18,5 @@ fn main() {
             println!("{:.0} {:.0} {:.0}", r, g, b)
         }
     }
+    eprintln!();
 }
