9ab921000814f15c4816100cd4fdeb4e2b50d734
Akihito Kirisaki <kirisaki@klara.works>
2024-02-16 20:19:33 +0900

    feat: output ppm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let width: u64 = 256;
+    let height: u64 = 256;
+
+    println!("P3");
+    println!("{:}", width);
+    println!("{:}", height);
+    println!("255");
+
+    for y in 0..height {
+        for x in 0..width {
+            let r = y as f64 / ((width - 1) as f64) * 255.99;
+            let g = x as f64 / ((height - 1) as f64) * 255.99;
+            let b = 0.25 * 255.99;
+
+            println!("{:.0} {:.0} {:.0}", r, g, b)
+        }
+    }
 }
