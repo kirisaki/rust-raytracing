ccdd331728efbe1c383ca98dba706d9d4af93142
Akihito Kirisaki <kirisaki@klara.works>
2024-02-16 22:06:46 +0900

    feat: define helpers

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,16 @@ fn main() {
         for x in 0..width {
             eprint!("\rx:{:5}, y:{:5}", x, y);
 
-            let r = y as f64 / ((width - 1) as f64) * 255.99;
-            let g = x as f64 / ((height - 1) as f64) * 255.99;
-            let b = 0.25 * 255.99;
-
-            println!("{:.0} {:.0} {:.0}", r, g, b)
+            let rgb = Vec3{x: y as f64 / ((width - 1) as f64), y: x as f64 / ((height - 1) as f64), z: 0.25};
+            write_color(rgb);
         }
     }
     eprintln!();
+}
+
+fn write_color(color: Color) {
+    let c = color * 255.99;
+    println!("{:.0} {:.0} {:.0}", c.x, c.y, c.z)
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -29,6 +31,9 @@ struct Vec3 {
     y: f64,
     z: f64,
 }
+
+type Color = Vec3;
+type Point = Vec3;
 
 impl Add for Vec3 {
     type Output = Self;
@@ -59,5 +64,19 @@ impl Div<f64> for Vec3 {
 
     fn div(self, t: f64) -> Self {
         Self{x: self.x / t, y: self.y / t, z: self.z / t}
+    }
+}
+
+impl Vec3 {
+    fn norm(self) -> f64 {
+        f64::sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
+    }
+
+    fn dot(self, other: Self) -> f64{
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    fn unit(self) -> Self {
+        self / self.norm()
     }
 }
