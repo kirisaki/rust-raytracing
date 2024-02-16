b5b1ca6626b001175112d6fcf0b87f179c1dec54
Akihito Kirisaki <kirisaki@klara.works>
2024-02-16 21:46:55 +0900

    feat: define Vec3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Sub, Mul, Div};
+
 fn main() {
     let width: u64 = 256;
     let height: u64 = 256;
@@ -19,4 +21,43 @@ fn main() {
         }
     }
     eprintln!();
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self{x: self.x + other.x, y: self.y + other.y, z: self.z + other.z}
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self{x: self.x - other.x, y: self.y - other.y, z: self.z - other.z}
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, t: f64) -> Self {
+        Self{x: self.x * t, y: self.y * t, z: self.z * t}
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Self;
+
+    fn div(self, t: f64) -> Self {
+        Self{x: self.x / t, y: self.y / t, z: self.z / t}
+    }
 }
